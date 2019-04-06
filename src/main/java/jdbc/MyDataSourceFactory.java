9ef0997f2e19effdec5f42149f235f0435f94b79
hendzabady04 <hendzabady04@gmail.com>
2019-04-06 17:42:19 +0200

    add config DB file

--- src/main/java/jdbc/MyDataSourceFactory.java
+++ src/main/java/jdbc/MyDataSourceFactory.java
@@ -5,10 +5,15 @@
  */
 package jdbc;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import javax.sql.DataSource;
+
 /**
  *
  * @author solo & hend
  */
 public class MyDataSourceFactory {
-    
+   
 }
