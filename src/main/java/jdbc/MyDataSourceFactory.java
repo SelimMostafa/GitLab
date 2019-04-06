3c0c1bab0bd1fb7cfdda432d4c78b4345ba0b032
Selim Mostafa <selimhamdy7@gmail.com>
2019-04-06 17:42:30 +0200

    implementing data source

--- src/main/java/jdbc/MyDataSourceFactory.java
+++ src/main/java/jdbc/MyDataSourceFactory.java
@@ -9,6 +9,31 @@ package jdbc;
  *
  * @author solo & hend
  */
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import javax.sql.DataSource;
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 public class MyDataSourceFactory {
-    
+
+    public static DataSource getMySQLDataSource() {
+        Properties properties = new Properties();
+        FileInputStream fileinput = null;
+        MysqlDataSource mysqlDS = null;
+
+        try {
+            fileinput = new FileInputStream("db.properties");
+            properties.load(fileinput);
+            mysqlDS = new MysqlDataSource();
+            // get the properties value
+            mysqlDS.setURL(properties.getProperty("MYSQL_DB_URL"));
+            mysqlDS.setUser(properties.getProperty("MYSQL_DB_USERNAME"));
+            mysqlDS.setPassword(properties.getProperty("MYSQL_DB_PASSWORD"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+            return mysqlDS;
+    }
 }
+
