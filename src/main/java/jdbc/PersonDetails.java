195bd75ec4c57dd3346dc9899501df81196479a7
hendzabady04 <hendzabady04@gmail.com>
2019-04-06 18:51:54 +0200

    deleted and new and prev added

--- src/main/java/jdbc/PersonDetails.java
+++ src/main/java/jdbc/PersonDetails.java
@@ -198,5 +198,148 @@ public class PersonDetails extends GridPane {
         updatebutton.setLayoutY(15.0);
         updatebutton.setMnemonicParsing(false);
         updatebutton.setText("Update");
+        
+          GridPane.setRowIndex(anchorPane0, 6);
+        anchorPane0.setPrefHeight(200.0);
+        anchorPane0.setPrefWidth(200.0);
+
+        newbutton.setLayoutX(49.0);
+        newbutton.setLayoutY(15.0);
+        newbutton.setMnemonicParsing(false);
+        newbutton.setText("New");
+          firstbutton.setLayoutX(148.0);
+        firstbutton.setLayoutY(15.0);
+        firstbutton.setMnemonicParsing(false);
+        firstbutton.setText("First");
+
+        firstbutton.setOnAction((e) -> {
+            firsView();
+
+        });
+
+        prevbutton.setLayoutX(213.0);
+        prevbutton.setLayoutY(15.0);
+        prevbutton.setMnemonicParsing(false);
+        prevbutton.setText("Previous");
+
+        prevbutton.setOnAction((e) -> {
+            try {
+                if (resultset.previous() == true) {
+                    textField.setText(resultset.getString(1));
+                    textField0.setText(resultset.getString(2));
+                    textField3.setText(resultset.getString(3));
+                    textField2.setText(resultset.getString(4));
+                    textField1.setText(resultset.getString(5));
+                    textField4.setText(resultset.getString(6));
+                    System.out.println("prev row");
+                } else {
+                    resultset.next();
+                    System.out.println("No more rows");
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        });
+
+
+        newbutton.setOnAction((e) -> {
+
+            try {
+                textField.clear();
+                textField0.clear();
+                textField1.clear();
+                textField2.clear();
+                textField3.clear();
+                textField4.clear();
+                resultset.moveToInsertRow();
+                new_flag = true;
+                updatebutton.setText("Add");
+
+            } catch (SQLException ex) {
+                System.out.println("This ID Already Exist, Check that you are entering a new employee");
+            }
+        });
+
+        GridPane.setColumnIndex(textField, 1);
+        textField.setPrefHeight(68.0);
+        textField.setPrefWidth(444.0);
+
+        GridPane.setColumnIndex(textField0, 1);
+        GridPane.setRowIndex(textField0, 1);
+        textField0.setPrefHeight(68.0);
+        textField0.setPrefWidth(444.0);
+
+        GridPane.setColumnIndex(textField1, 1);
+        GridPane.setRowIndex(textField1, 4);
+        textField1.setPrefHeight(68.0);
+        textField1.setPrefWidth(444.0);
+
+        GridPane.setColumnIndex(textField2, 1);
+        GridPane.setRowIndex(textField2, 3);
+        textField2.setPrefHeight(68.0);
+        textField2.setPrefWidth(444.0);
+
+        GridPane.setColumnIndex(textField3, 1);
+        GridPane.setRowIndex(textField3, 2);
+        textField3.setPrefHeight(68.0);
+        textField3.setPrefWidth(444.0);
+
+        GridPane.setColumnIndex(textField4, 1);
+        GridPane.setRowIndex(textField4, 5);
+        textField4.setPrefHeight(68.0);
+        textField4.setPrefWidth(444.0);
+
+        getColumnConstraints().add(columnConstraints);
+        getColumnConstraints().add(columnConstraints0);
+        getRowConstraints().add(rowConstraints);
+        getRowConstraints().add(rowConstraints0);
+        getRowConstraints().add(rowConstraints1);
+        getRowConstraints().add(rowConstraints2);
+        getRowConstraints().add(rowConstraints3);
+        getRowConstraints().add(rowConstraints4);
+        getRowConstraints().add(rowConstraints5);
+        getChildren().add(label);
+        getChildren().add(label0);
+        getChildren().add(label1);
+        getChildren().add(label2);
+        getChildren().add(label3);
+        getChildren().add(label4);
+        getChildren().add(label5);
+        anchorPane.getChildren().add(updatebutton);
+        anchorPane.getChildren().add(deletebutton);
+        anchorPane.getChildren().add(firstbutton);
+        anchorPane.getChildren().add(prevbutton);
+        anchorPane.getChildren().add(nextbutton);
+        anchorPane.getChildren().add(lastbutton);
+        getChildren().add(anchorPane);
+        anchorPane0.getChildren().add(newbutton);
+        getChildren().add(anchorPane0);
+        getChildren().add(textField);
+        getChildren().add(textField0);
+        getChildren().add(textField1);
+        getChildren().add(textField2);
+        getChildren().add(textField3);
+        getChildren().add(textField4);
+        firsView();
+
+    }
+
+    private void firsView() {
+        try {
+            resultset.first();
+            textField.setText(resultset.getString(1));
+            textField0.setText(resultset.getString(2));
+            textField3.setText(resultset.getString(3));
+            textField2.setText(resultset.getString(4));
+            textField1.setText(resultset.getString(5));
+            textField4.setText(resultset.getString(6));
+            System.out.println("Show first");
+        } catch (SQLException ex) {
+            System.out.println("No more rows");
+        }
+        
+        
+        
+        
     }
 }
